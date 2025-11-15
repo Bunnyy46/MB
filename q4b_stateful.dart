cfcc448942dfd20f7c3713f87719ae23d51c0513
Bunnyy46 <editjunkie3@gmail.com>
2025-11-15 10:23:01 +0530

    Update q4b_stateful.dart

--- q4b_stateful.dart
+++ q4b_stateful.dart
@@ -1,30 +1,37 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(home: CounterApp()));
-
-class CounterApp extends StatefulWidget {
-  @override
-  State<CounterApp> createState() => _CounterAppState();
+import'package:flutter/material.dart';
+void main()=>runApp(MaterialApp(home:MyApp()));
+class MyApp extends StatefulWidget{
+  State<MyApp> createState()=>_MyAppState();
 }
-
-class _CounterAppState extends State<CounterApp> {
-  int c = 0;
-
-  @override
-  Widget build(context) => Scaffold(
-        appBar: AppBar(title: Text("Hello")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Stateless Text"),
-              Text("Count: $c"),
-              ElevatedButton(
-                onPressed: () => setState(() => c++),
-                child: Text("Increase"),
-              )
-            ],
-          ),
-        ),
-      );
+class _MyAppState extends State<MyApp>{
+  int count=0;
+  Widget build(BuildContext context){
+    return MaterialApp(
+    home:Scaffold(
+    appBar:AppBar(title:Text("hello")),
+      body:Center(
+      child:Column(
+      mainAxisAlignment:MainAxisAlignment.center,
+        children:[
+          MyStatelessText(),
+        Text("Count:$count"),
+          ElevatedButton(
+          onPressed:(){
+            setState((){
+              count++;
+            });
+          },
+            child:Text("increse"),
+            ),
+        ],
+      ),
+      ),
+    ),
+    );
+  }
+}
+class MyStatelessText extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Text("i never trust");
+  }
 }
