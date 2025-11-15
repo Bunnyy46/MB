52c8cbe64eee6b60f9d758c6cebffdefb3f68da8
Bunnyy46 <editjunkie3@gmail.com>
2025-11-15 12:52:29 +0530

    Update q2a_column_row_stack.dart

--- q2a_column_row_stack.dart
+++ q2a_column_row_stack.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Example2A extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Column Row Stack")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.star, color: Colors.orange),
-                Icon(Icons.favorite, color: Colors.blue),
-                Icon(Icons.thumb_up, color: Colors.red),
-              ],
-            ),
-            SizedBox(height: 20),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(height: 20, width: 20, color: Colors.black),
-                Container(height: 10, width: 10, color: Colors.cyan),
-              ],
-            ),
-          ],
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:Scaffold(
+        appBar:AppBar(title: Text("column & row & stack")),
+        body:Center(
+        child:Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+         children:[
+           Row(
+             mainAxisAlignment:MainAxisAlignment.center,
+             children:[
+               Icon(Icons.star,color:Colors.orange),
+               Icon(Icons.favorite,color:Colors.blue),
+               Icon(Icons.thumb_up,color:Colors.red),
+               
+             ],
+           ),
+           SizedBox(height:20),
+           Stack(
+             alignment:Alignment.center,
+             children:[
+               Container(height:20,width:20,color:Colors.black),
+               Container(height:10,width:10,color:Colors.cyan),
+             ],
+           ),
+         ],
+          
+        ),
         ),
       ),
     );
